586d545d8fc79b5c6e8b2774f3f3ab385f5c933e
wondaero <wondaero@nate.com>
2025-05-03 11:58:42 +0900

    Revert "테스트겸 버튼 기능 추가"
    This reverts commit 80ca97f5ff429c5880a8e8eab086d18683c37b30.

--- src/app/admin/stage/page.tsx
+++ src/app/admin/stage/page.tsx
@@ -277,21 +277,6 @@ export default function AdminStage() {
     );
   }
 
-  const addStage = () => {
-    setItems((prev) => {
-      return [
-        { id: 'aaa', content: "아이템 1", isDragEnabled: false },
-        ...prev
-      ];
-    });
-    
-  }
-  const delStage = (id: string) => {
-    setItems((items) =>
-      items.filter((item) => item.id !== id)
-    );
-  }
-
   
 
   return (
@@ -305,7 +290,7 @@ export default function AdminStage() {
             <div className="list-top">
               <strong>총 <span>10</span> Stage</strong>
               <div>
-                <button onClick={addStage}>추가</button>
+                <button>추가</button>
               </div>
             </div>
             <div className="table-wrapper">
